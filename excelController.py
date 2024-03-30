42dfda0c1908b9741ca9ead44cbbeaeeae59082d
Maciej Trzuskowski <miketrusky270696@gmail.com>
2024-03-30 22:45:38 +0100

    [UI] GUI window: now 5 buttons & 2 labels. [ExcelOperations] Possible to remove player row. [Refactor] Change to not use hardcoded file name.

--- excelController.py
+++ excelController.py
@@ -2,43 +2,54 @@ import openpyxl
 import xlwings as xw
 import csv
 
-positionColumn = 1 #const
-teamColumn = 2 #const
-playerColumn = 3 #const
-fileName = "Barrow.xlsx" #read from config/arguments/something
-allColumnsCount = 27
-
-#TODO column by index, or finding index by column name? 
-
+#region Const Values
+positionColumn = 1
+teamColumn = 2
+playerColumn = 3
 positionMainValueColumn = 10
 positionSecondaryValueColumn = 12
 positionProgressColumn = 14
 CAValueColumn = 21
 CAProgressColumn = 22
+allColumnsCount = 27
+csvTemplateFile = "template.csv"
+#endregion
 
+#TODO column by index, or finding index by column name? 
 #TODO use only one: xlwings or openpyxl?
 
 class Helper:
+    #only for openpyxl
     def find_row_by_value(self, sheet, column, min_row_value, value):        
         for index, row in enumerate(sheet.iter_rows(min_row = min_row_value, max_row=sheet.max_row, values_only=True)):
             if row[column-1] == value:
                 return index        
         return None            
+
+    #only for xlwings
+    def remove_row(self, sheet, row):
+        sheet.range((row - 1, 1)).api.EntireRow.Delete()
         
 class OpenpyxlController:
+    def __init__(self, excelFileName):
+        self.excelFileName = excelFileName
+
     def create_sheet(self):
-        self.wb = openpyxl.load_workbook(fileName)
+        self.wb = openpyxl.load_workbook(self.excelFileName)
         return self.wb.active
 
     def close_controller(self):
         self.wb.close()    
 
 class XlwingsController:
+    def __init__(self, excelFileName):
+        self.excelFileName = excelFileName
+
     def create_sheet(self, useApp):
         self.useApp = useApp
         if useApp:            
             self.app = xw.App(visible=False) #use it for "silent" open & close excel
-        self.wb = xw.Book(fileName)
+        self.wb = xw.Book(self.excelFileName)
         return self.wb.sheets.active
     
     def close_controller(self, closeFile):
@@ -64,7 +75,7 @@ class CsvController:
         return data
     
     def createTemplateFile(self):
-        valuesTemplateFilename = 'template.csv'
+        valuesTemplateFilename = csvTemplateFile
         data = [
             ['Position', 'Team', 'Name', 'Age', 'Country', 'NONE', 'ToolRating_1', 'ToolRating_2', 'NONE', 'PositionMain_Name', 'PositionMain_Value', 'PositionSec_Name', 'PositionSec_Value', 'NONE', 
             'Progress', 'NONE', 'NONE', 'Determination', 'Potential', 'NoPermission', 'NONE', 'CA', 'CAChange', 'PA', 'PlayerStatus', 'RaportStatus', 'Info']    
@@ -72,11 +83,12 @@ class CsvController:
         self.create_csv(valuesTemplateFilename, data)
 
 class ExcelModificationsController:
-    def __init__(self):
+    def __init__(self, excelFileName): 
         self.helper = Helper()
-        self.openpyxlController = OpenpyxlController()
-        self.xlwingsController = XlwingsController()    
+        self.openpyxlController = OpenpyxlController(excelFileName)
+        self.xlwingsController = XlwingsController(excelFileName)    
         self.csvController = CsvController()            
+        self.csvController.createTemplateFile()
 
     def find_teamPart_row(self, positionValue, teamValue):
         sheet = self.openpyxlController.create_sheet()
@@ -120,8 +132,8 @@ class ExcelModificationsController:
         self.xlwingsController.close_controller(True)
         return data
     
-    def insert_player_by_file(self):
-        readFileData = self.csvController.read_csv("playerInfo.csv")
+    def insert_player_by_file(self, csvFileName):
+        readFileData = self.csvController.read_csv(csvFileName)
         searchValue = readFileData[1][2]
         playerRow = self.get_player_row(searchValue)
         if playerRow is not None:
@@ -136,13 +148,14 @@ class ExcelModificationsController:
             newRow = self.insert_row(readFileData[1][0], readFileData[1][1])
             self.update_row_values(newRow + 1, readFileData[1])        
 
-    def update_player_by_file(self):
-        #TODO check if player exist
-        readTemplateData = self.csvController.read_csv("template.csv")
-        readPlayerData = self.csvController.read_csv("playerInfoOnlyFew.csv")
+    def update_player_by_file(self, csvFileName):
+        #TODO check if player exist        
+        readTemplateData = self.csvController.read_csv(csvTemplateFile)
+        readPlayerData = self.csvController.read_csv(csvFileName)
         playerRow = self.get_player_row(readPlayerData[1][readPlayerData[0].index("Name")])
         columnsIndexes = []
         values = []
+        #TODO update progress and CA change
         for word in readPlayerData[0]:
             if word in readTemplateData[0]:                
                 columnsIndexes.append(readTemplateData[0].index(word))
@@ -150,12 +163,19 @@ class ExcelModificationsController:
         columnsIndexes = [index + 1 for index in columnsIndexes]                
         self.update_columns_values(playerRow + 1, columnsIndexes, values)
 
+    def delete_player_by_file(self, csvFileName):
+        readPlayerData = self.csvController.read_csv(csvFileName)
+        print(readPlayerData[1][0])
+        playerRow = self.get_player_row(readPlayerData[1][readPlayerData[0].index("Name")])
+        sheet = self.xlwingsController.create_sheet(True)
+        self.helper.remove_row(sheet, playerRow)
+
     def calculate_difference(self, previousPlayerData, currentPlayerData, columnValue):
         previousValue = float(previousPlayerData[columnValue])
         currentValue = float(currentPlayerData[1][columnValue]) 
         return (currentValue - previousValue)
 
-#excelModificationsController = ExcelModificationsController()
+#excelModificationsController = ExcelModificationsController("Barrow.xlsx")
 #excelModificationsController.insert_row("BR", "REZERWA")           
 #excelModificationsController.insert_player_by_file()
 #excelModificationsController.update_player_by_file()
--- viewController.py
+++ viewController.py
@@ -2,12 +2,11 @@ from tkinter import *
 import customtkinter
 from tkinter import filedialog
 from excelController import ExcelModificationsController
+import os
 
 class ViewApp(customtkinter.CTk):
     def __init__(self):
-        super().__init__()
-
-        self.excel_modifications_controller = ExcelModificationsController()
+        super().__init__()        
 
         customtkinter.set_appearance_mode("dark")
         customtkinter.set_default_color_theme("blue")
@@ -15,22 +14,62 @@ class ViewApp(customtkinter.CTk):
         self.title("FM Excel Controller App")
         self.geometry("300x400")
 
-        self.choose_file_button = customtkinter.CTkButton(self, text="Choose csv file", command=self.on_choose_file_button_click)
-        self.choose_file_button.place(relx=0.5, rely=0.2, anchor=CENTER)
+        self.choose_excel_file_button = customtkinter.CTkButton(self, text="Choose excel file", command=self.on_choose_excel_file_button_click)
+        self.choose_excel_file_button.place(relx=0.5, rely=0.2, anchor=CENTER)
 
-        self.file_label = customtkinter.CTkLabel(self, text="")
-        self.file_label.place(relx=0.5, rely=0.4, anchor=CENTER)
+        self.excel_file_label = customtkinter.CTkLabel(self, text="Excel file name: ")
+        self.excel_file_label.place(relx=0.1, rely=0.3) 
+        self.excel_file_name_label = customtkinter.CTkLabel(self, text="none")
+        self.excel_file_name_label.place(relx=0.4, rely=0.3)
 
+        self.choose_csv_file_button = customtkinter.CTkButton(self, text="Choose csv file", command=self.on_choose_csv_file_button_click)
+        self.choose_csv_file_button.place(relx=0.5, rely=0.5, anchor=CENTER)
+        self.choose_csv_file_button.configure(state="disabled")
+
+        self.csv_file_label = customtkinter.CTkLabel(self, text="CSV file name: ")
+        self.csv_file_label.place(relx=0.1, rely = 0.6) 
+        self.csv_file_name_label = customtkinter.CTkLabel(self, text="none")
+        self.csv_file_name_label.place(relx=0.4, rely=0.6)
+        
         self.update_excel_button = customtkinter.CTkButton(self, text="Update player with file!", command=self.on_update_excel_button_click)
-        self.update_excel_button.place(relx=0.5, rely=0.6, anchor=CENTER)
+        self.update_excel_button.place(relx=0.5, rely=0.75, anchor=CENTER)
+        self.update_excel_button.configure(state="disabled")
 
-    def on_choose_file_button_click(self):
+        self.insert_player_excel_button = customtkinter.CTkButton(self, text="Insert player with file!", command=self.on_insert_player_excel_button_click)
+        self.insert_player_excel_button.place(relx=0.5, rely=0.85, anchor=CENTER)
+        self.insert_player_excel_button.configure(state="disabled")
+
+        self.delete_player_excel_button = customtkinter.CTkButton(self, text="Delete from with file!", command=self.on_delete_player_excel_button_click)
+        self.delete_player_excel_button.place(relx=0.5, rely=0.95, anchor=CENTER)
+        self.delete_player_excel_button.configure(state="disabled")
+
+    def on_choose_excel_file_button_click(self):
         file_path = filedialog.askopenfilename()
-        self.file_label.configure(text=file_path)        
+        if file_path:
+            self.excel_file_name = os.path.basename(file_path)
+        self.excel_file_name_label.configure(text=self.excel_file_name)        
+        self.choose_csv_file_button.configure(state="normal")
 
-    def on_update_excel_button_click(self):
-        print("Update player by file")
-        #self.excel_modifications_controller.update_player_by_file()
+    def on_choose_csv_file_button_click(self):
+        file_path = filedialog.askopenfilename()
+        if file_path:
+            self.csv_file_name = os.path.basename(file_path)
+        self.csv_file_name_label.configure(text=self.csv_file_name)        
+        self.update_excel_button.configure(state="normal")        
+        self.insert_player_excel_button.configure(state="normal")
+        self.delete_player_excel_button.configure(state="normal")
+
+    def on_update_excel_button_click(self):  
+        self.excel_modifications_controller = ExcelModificationsController(self.excel_file_name)
+        self.excel_modifications_controller.update_player_by_file(self.csv_file_name)
+
+    def on_insert_player_excel_button_click(self):
+        self.excel_modifications_controller = ExcelModificationsController(self.excel_file_name)
+        self.excel_modifications_controller.insert_player_by_file(self.csv_file_name)
+
+    def on_delete_player_excel_button_click(self):
+        self.excel_modifications_controller = ExcelModificationsController(self.excel_file_name)
+        self.excel_modifications_controller.delete_player_by_file(self.csv_file_name)
 
 viewApp = ViewApp()
 viewApp.mainloop()
